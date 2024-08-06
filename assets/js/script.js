9e5e5c5181304328996795ed8a77a299830fdce2
yafoo <yafoo93@gmail.com>
2024-08-06 00:09:44 +0000

    created most files and style them

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const section = document.querySelector('section');
+    section.addEventListener('click', function() {
+        alert('Welcome to the Luxurious Website!');
+    });
+});
